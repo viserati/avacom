eb436f665fde79d7a67166d74283f3c6a09d2afe
Keith Turner <turner@keithturner.com>
2018-06-03 03:19:38 +0000

    Created scaffold for the Asset Acquisition Application. Tested Model.

---
+++ db/migrate/20180602000944_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.string :image
+      t.string :_url
+      t.decimal :price, precision: 8, scale: 2
+
+      t.timestamps
+    end
+  end
+end
